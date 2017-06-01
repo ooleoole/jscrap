7cf046cfac29a7587ccd5524ac9f2fe0090e7034
Ola Popp <olapopp85@gmail.com>
2017-06-01 15:25:25 +0200

    awd

--- app.js
+++ app.js
@@ -15,20 +15,42 @@ var g = GetIncrement(1);
 // document.writeln(g() + "<br/>");
 // document.writeln(g() + "<br/>");
 
-document.getElementById("li1").innerHTML=g();
-document.getElementById("li2").innerHTML=g();
-document.getElementById("li3").innerHTML=g();
-document.getElementById("li4").innerHTML=g();
+document.getElementById("li1").innerHTML = g();
+document.getElementById("li2").innerHTML = g();
+document.getElementById("li3").innerHTML = g();
+document.getElementById("li4").innerHTML = g();
 
-function addSomeText(){
+function addSomeText() {
 
     var text = document.getElementById("input").value;
     var ul = document.getElementById("ul");
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(text));
-    li.setAttribute("id","li"+g());
+    li.setAttribute("id", "li" + g());
     ul.appendChild(li);
-    document.getElementById("input").value="";
+    document.getElementById("input").value = "";
+}
 
+
+function Emp(name, salary) {
+
+    this.name = name;
+    this.salary = salary;
+
+    this.issuePaycheck = function () {
+        var monthly = this.salary / 12;
+        console.log(this.name + " " + monthly);
+    }
 
 }
+Emp.company = "OdenBidlear"
+Emp.prototype.issuePaycheck = function () {
+    var monthly = this.salary / 12;
+    console.log(this.name + " " + monthly + " " + this.company);
+}
+
+var emp = new Emp("ola", 123456789)
+emp.issuePaycheck();
+
+var emp2 = new Emp("carin", 12345679)
+emp2.issuePaycheck();
